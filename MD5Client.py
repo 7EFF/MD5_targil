e22a69d1b5182d32690ed8a0a7b0d38e62ff1c9d
7EFF <90554401+7EFF@users.noreply.github.com>
2021-11-09 11:01:19 +0200

    Update MD5Client.py

--- MD5Client.py
+++ MD5Client.py
@@ -8,34 +8,33 @@ def Recieve(my_socket):
     str2=""
     for s in rlist:
         str2 = s.recv(1024).decode()
-
-    after(10, lambda: Recieve(my_socket))
+    return str2
 
 
 def main():
     my_socket = socket.socket()
     my_socket.connect(('127.0.0.1', 5555))
     str2=""
+    str2 = my_socket.recv(1024).decode()
     while True:
 
         #str2 = input("enter md5 number")
         print("the original number is:", end="")
         i = 0
+        message = "Ready for work"
+        my_socket.send(message.encode())
         while True:
-            str2=""
-            str2=my_socket.recv(1024).decode()
-            if str2!="":
-                while True:
-                    md = str(i)
-                    for j in range(len(md), 11):
-                        md = md.zfill(j)
-                        print(md)
-                        result = hashlib.md5(md.encode())
-                        if (result.hexdigest() == str2):
-                            print(md)
-                            my_socket.send(md.encode)
-                            return
-                    i = i + 1
+            md = str(i)
+            for j in range(len(md), 11):
+                md = md.zfill(j)
+                #print(md)
+                result = hashlib.md5(md.encode())
+                if (result.hexdigest() == str2):
+                    print(md)
+                    my_socket.send(md.encode())
+                    #time.sleep(0.05)
+                    return
+            i = i + 1
 
 
 if __name__ == '__main__':
