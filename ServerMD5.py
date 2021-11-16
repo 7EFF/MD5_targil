138593bfff1bd94e6f288fd519f58301244fd388
7EFF <90554401+7EFF@users.noreply.github.com>
2021-11-16 11:04:43 +0200

    Update ServerMD5.py

--- ServerMD5.py
+++ ServerMD5.py
@@ -2,6 +2,8 @@ import socket
 import select
 import time
 import threading
+import queue
+
 
 MAX_MSG_LENGTH = 1024
 SERVER_PORT = 5555
@@ -12,7 +14,25 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
 server_socket.listen()
 print("Listening for clients...")
-client_sockets = []
+client_sockets=[]
+
+def socket_work(socket,number,queue,client_address):
+    encryp = "Not found"
+    while encryp =="Not found" and queue.full():
+        socket.send(number.encode())
+        min = queue.pop(0)
+        #print(min)
+        socket.send(str(min).encode())
+        req = socket.recv(1024).decode()
+        print(client_address, " ", req)
+        if req == "Ready for work":
+            encryp = socket.recv(1024).decode()
+            #print(encryp)
+            if encryp != "Not found":
+                print("the encypted number is: ", encryp)
+                queue.clear()
+                return
+
 
 
 def send_to(thread, socket):
@@ -23,7 +43,9 @@ def send_to(thread, socket):
 
 def main():
     count_client = 0
-    list =[range(0,999999999,1000000)]
+    queue = []
+    for i in range(0, 999999999, 1000000):
+        queue.append(i)
     number = input("Enter a number you want to decrypt")
     while True:
         rlist, wlist, xlist = select.select([server_socket] + client_sockets, client_sockets, [])
@@ -32,20 +54,12 @@ def main():
                 connection, client_address = current_socket.accept()
                 print("New client joined!", client_address)
                 client_sockets.append(connection)
-                req=""
-
-                for socket in client_sockets:
-                    socket.send(number.encode())
-                    req = socket.recv(1024).decode()
-                    print(client_address, " ", req)
-                    if req == "Ready for work":
-                        encryp = socket.recv(1024).decode()
-                        print("the encypted number is: ", encryp)
-                        return
-                    else:
-                        continue
-
+                #for socket in client_sockets:
+                    #socket_work(socket, number, queue, client_address)
+                x = threading.Thread(target=socket_work, args=(connection,number,queue,client_address))
+                x.start()
 
 
 if __name__ == '__main__':
     main()
+
