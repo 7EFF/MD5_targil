6d80ed9aa7e79838a637f83a3476e2304b09b471
7EFF <90554401+7EFF@users.noreply.github.com>
2021-11-16 11:04:59 +0200

    Update MD5Client.py

--- MD5Client.py
+++ MD5Client.py
@@ -14,28 +14,29 @@ def Recieve(my_socket):
 def main():
     my_socket = socket.socket()
     my_socket.connect(('127.0.0.1', 5555))
-    str2=""
-    str2 = my_socket.recv(1024).decode()
     while True:
-
+        str2 = my_socket.recv(1024).decode()
+        min = int(my_socket.recv(1024).decode())
         #str2 = input("enter md5 number")
-        print("the original number is:", end="")
-        i = 0
+        i = min
+        print(min)
         message = "Ready for work"
         my_socket.send(message.encode())
-        while True:
+        while i<min+1000000:
             md = str(i)
             for j in range(len(md), 11):
                 md = md.zfill(j)
                 #print(md)
                 result = hashlib.md5(md.encode())
                 if (result.hexdigest() == str2):
+                    print("the original number is:", end="")
                     print(md)
                     my_socket.send(md.encode())
                     #time.sleep(0.05)
                     return
             i = i + 1
-
+        msg = "Not found"
+        my_socket.send(msg.encode())
 
 if __name__ == '__main__':
     main()
